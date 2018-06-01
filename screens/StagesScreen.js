889f4ad2f718206f3328dc322a5c2a84c30b549c
Loi Tran <lvt15@my.fsu.edu>
2018-06-01 18:49:16 +0800

    Completed showing avatar_url photo in the header and post body

--- screens/StagesScreen.js
+++ screens/StagesScreen.js
@@ -1,21 +1,19 @@
 
 import React from 'react';
 import {
-  Image,
   Platform,
   ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
+  Image
 } from 'react-native';
 
 import axios from 'axios';
 
 import { API_ROOT } from '../constants/ApiConfig';
-import { Card, CardSection } from '../components/common'
-
-import { AsyncStorage } from 'react-native';
+import { Card, CardSection } from '../components/common';
 
 import client from '../utils/client';
 
@@ -33,55 +31,55 @@ export default class StagesScreen extends React.Component {
     })
   }
 
-
-  static navigationOptions = {
-    title: 'Venues'
-  };
-
   render() {
-    const { lists } = this.state
+    const { posts } = this.state
     const {
       headerContainerStyle,
       headerTextStyle,
-      container
+      container,
+      thumbnailStyle
      } = styles;
 
-    return (
-      <ScrollView style={container}>
-        { this.state.posts && this.state.posts.map(post => {
-            return (
-              <View style={container} key={post.id}>
-                {/* <CardSection>{post.body}</CardSection> */}
-                <Card>
-                  <CardSection>
-                    <View style={headerContainerStyle}>
-                      <Text style={headerTextStyle}>{post.user.email}</Text>
-                    </View>
-                  </CardSection>
+    if (posts) {
+      return (
+        <ScrollView style={container}>
+          { posts && posts.map(post => {
+              console.log('post:', post)
+              return (
+                <View style={container} key={post.id}>
+                  <Card>
+                    <CardSection>
+                      <View style={headerContainerStyle}>
+                        <Image
+                          style={thumbnailStyle}
+                          source={{ uri: post.user.avatar_url }}
+                        />
+                      </View>
+                      <View style={headerContainerStyle}>
+                        <Text style={headerTextStyle}>{post.user.full_name}</Text>
+                      </View>
+                    </CardSection>
 
-                  <CardSection>
-                    <Text>
-                      {post.body}
-                    </Text>
-                  </CardSection>
-                </Card>
-
-              </View>
-              )
-            }
-          )
-        }
-      </ScrollView>
-    );
+                    <CardSection>
+                      <Text>
+                        {post.body}
+                      </Text>
+                    </CardSection>
+                  </Card>
+                </View>
+                )
+              }
+            )
+          }
+        </ScrollView>
+      )
+    } else {
+      <div>Empty</div>
+    }
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
   headerContainerStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
@@ -89,5 +87,14 @@ const styles = StyleSheet.create({
   headerTextStyle: {
     fontSize: 25,
     fontWeight: '600'
+  },
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
   }
 });

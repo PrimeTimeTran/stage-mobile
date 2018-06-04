0a0f5a69623909db507c5026cebd88abd89aa6ab
Loi Tran <lvt15@my.fsu.edu>
2018-06-04 18:30:29 +0800

    Styling

--- screens/StagesScreen.js
+++ screens/StagesScreen.js
@@ -94,6 +94,7 @@ export default class StagesScreen extends React.Component {
     const { stages } = this.state
     const {
       cardHeaderStyle,
+      descriptionStyle,
       headerContainerStyle,
       headerTextStyle,
       thumbnailStyle
@@ -140,12 +141,12 @@ export default class StagesScreen extends React.Component {
                     </CardSection>
                     <CardSection>
                         <View style={{flex: 1, flexDirection: 'row'}}>
-                          <View style={{justifyContent: 'space-between', paddingLeft: 5}}>
+                          <View style={descriptionStyle}>
                             <Icon name='place' color='#276FBF'/>
                             <Icon name='mobile' type='font-awesome' color='#276FBF'/>
                             <Icon name='users' type='font-awesome' color='#276FBF' size={15}/>
                           </View>
-                          <View style={{justifyContent: 'space-between', paddingLeft: 5}}>
+                          <View style={descriptionStyle}>
                             <Text>{stage.address}, {stage.city}, {stage.country}</Text>
                             <Text>{stage.phone}</Text>
                             <Text>{Math.floor(Math.random() * Math.floor(1000))} <Text style={{color: 'green'}}>active users</Text></Text>
@@ -214,5 +215,9 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     borderRadius: 25
+  },
+  descriptionStyle: {
+    justifyContent: 'space-between',
+    paddingLeft: 5
   }
 })

864e5bb97c65f56a58782a742d740258159f38b6
Loi Tran <lvt15@my.fsu.edu>
2018-06-08 15:35:03 +0800

    Add style to drawer

--- components/Drawer.js
+++ components/Drawer.js
@@ -1,30 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   View,
   Text,
-  Dimensions,
-  Platform
-} from 'react-native';
+  Dimensions
+} from 'react-native'
 
 let { width, height } = Dimensions.get('window')
 
 export default class Drawer extends Component {
   render() {
     const { drawerStyle } = styles
+    const open = this.props.navigation.state.isDrawerOpen
+    const style = open ? drawerStyle : ''
     return (
-      <View style={drawerStyle}>
+      <View style={style}>
         <View >
           <Text style={{color: 'red'}}> Drawer Component </Text>
         </View>
       </View>
-    );
+    )
   }
 }
 
 styles = {
   drawerStyle: {
+    backgroundColor: '#333333',
     height: height,
-    width: width * .75,
+    width: width * .80,
     alignItems: 'center',
     justifyContent: 'center'
   }

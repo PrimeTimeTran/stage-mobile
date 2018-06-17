08b5f918526b77375d892eae018bc50fa2b2bc4f
Loi Tran <lvt15@my.fsu.edu>
2018-06-17 13:07:24 +0700

    Fix bug in client.js

--- utils/client.js
+++ utils/client.js
@@ -2,15 +2,16 @@ import axios from 'axios'
 import { AsyncStorage } from 'react-native'
 
 const client = async () => {
-  let hardToken = 'eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjozLCJleHAiOjE1NTk1Nzk4MTJ9.yQnmTqPo5kTHOFrkbR5f4YXuYJQo3t5WXkYH8zS-Oa-b6OlridEu9McSlwVMN5yVg9OD8L6t0b6MBbWwonAwVg'
+  // let hardToken = 'eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjozLCJleHAiOjE1NTk1Nzk4MTJ9.yQnmTqPo5kTHOFrkbR5f4YXuYJQo3t5WXkYH8zS-Oa-b6OlridEu9McSlwVMN5yVg9OD8L6t0b6MBbWwonAwVg'
   let token = await AsyncStorage.getItem('auth_token')
-  if (!token) {
-    token = hardToken
+
+  if (token) {
+    token = token.slice(1, -1)
   }
 
   const defaultOptions = {
     headers: {
-      Authorization: token ? `Token ${hardToken}` : ''
+      Authorization: token ? `Token ${token}` : ''
     }
   }
 
